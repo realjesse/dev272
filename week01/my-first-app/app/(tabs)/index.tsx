78ede6051658f4f48bd523dda79968eee172d6a5
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-17 21:29:40 -0700

    Flex View element

--- week01/my-first-app/app/(tabs)/index.tsx
+++ week01/my-first-app/app/(tabs)/index.tsx
@@ -6,14 +6,16 @@ import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <ScrollView>
-        
+
       </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1
+  }
 });
