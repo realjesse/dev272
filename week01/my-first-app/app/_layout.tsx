d8ab9a89ac60a318b9d2afbec8d9c4c23ba3e5a9
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-27 16:41:51 -0700

    Add context for theme

--- week01/my-first-app/app/_layout.tsx
+++ week01/my-first-app/app/_layout.tsx
@@ -1,14 +1,23 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { GluestackUIProvider, ModeType } from "@/components/ui/gluestack-ui-provider";
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { createContext, useEffect } from 'react';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+type ThemeContextType = {
+  colorMode: ModeType;
+  toggleColorMode: (mode: ModeType) => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  colorMode: "light",
+  toggleColorMode: () => {},
+});
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
