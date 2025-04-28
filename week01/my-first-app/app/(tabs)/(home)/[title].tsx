4a3f523c9c9515b24942caaedf9db8260aeec529
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-27 17:57:56 -0700

    Create dynamic page

---
+++ week01/my-first-app/app/(tabs)/(home)/[title].tsx
@@ -0,0 +1,16 @@
+import { Box } from "@/components/ui/box";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { useLocalSearchParams, useRouter } from "expo-router";
+
+export default function DetailsScreen() {
+    const router = useRouter();
+    const { title } = useLocalSearchParams<{title: string}>()
+
+    return (
+        <Box className="flex-1 p-4">
+            <Heading size="xl" className="self-center">Dynamic Page: {title}</Heading>
+            <Button onPress={() => router.back()}>Back</Button>
+        </Box>
+    )
+}
