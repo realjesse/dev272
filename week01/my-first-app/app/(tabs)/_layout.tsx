774023b02e65a623036e44e0943f0728627b1c3f
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-28 21:58:46 -0700

    Delete original homepage

--- week01/my-first-app/app/(tabs)/_layout.tsx
+++ week01/my-first-app/app/(tabs)/_layout.tsx
@@ -48,12 +48,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarButton: () => null,
-        }}
-      />
     </Tabs>
   );
 }
