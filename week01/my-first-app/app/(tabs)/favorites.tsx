0b0afbe87827a99de18087083dc588e8b0a5e2a8
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-29 19:30:13 -0700

    Add Text Gluestack component

--- week01/my-first-app/app/(tabs)/favorites.tsx
+++ week01/my-first-app/app/(tabs)/favorites.tsx
@@ -1,18 +1,12 @@
-import { StyleSheet, Image, Platform } from 'react-native';
-
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import { Box } from '@/components/ui/box';
 import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
 
 export default function Favorites() {
   return (
     <Box>
       <Heading size="4xl" className="self-center">Favorites</Heading>
+      <Text size="xl" className="self-center">Your list of favorite paintings below...</Text>
     </Box>
   );
 }
