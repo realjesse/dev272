7629ba0b23c4722e03b2524fe331ca44969ce582
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-03 10:30:28 -0700

    Add SafeAreaView, Stack.Screen title name

--- week01/my-first-app/app/(tabs)/(home)/[title].tsx
+++ week01/my-first-app/app/(tabs)/(home)/[title].tsx
@@ -2,8 +2,8 @@ import { Box } from "@/components/ui/box";
 import { Heading } from "@/components/ui/heading";
 import { Image } from "@/components/ui/image";
 import { Text } from "@/components/ui/text";
-import { useLocalSearchParams, useRouter } from "expo-router";
-import { Button } from "react-native";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { Button, SafeAreaView } from "react-native";
 import { ButtonText, Button as GlueButton } from "@/components/ui/button"; 
 
 export default function DetailsScreen() {
@@ -17,19 +17,26 @@ export default function DetailsScreen() {
     }>()
 
     return (
-        <Box className="flex-1 p-4">
-            <Image
-                size="2xl"
-                source={{uri: wikipediaLink}}
-                alt="image of painting"
-                className="self-center"
-            />
-            <Heading size="xl" className="self-center">{name}</Heading>
-            <Text className="self-center" size="xl">{artist}, {year}</Text>
-            <GlueButton>
-                <ButtonText>Add To Favorites</ButtonText>
-            </GlueButton>
-            <Button title="Go Back" onPress={() => router.back()}/>
-        </Box>
+        <SafeAreaView className='flex-1 bg-white dark:bg-zinc-700'>
+            <Box className="p-4 m-4 dark:bg-[#151718] bg-white max-h-screen-safe items-center rounded-md">
+                <Stack.Screen
+                    options={{
+                        title: name,
+                    }}
+                />
+                <Image
+                    size="2xl"
+                    source={{uri: wikipediaLink}}
+                    alt="image of painting"
+                    className="self-center"
+                />
+                <Heading size="xl" className="self-center">{name}</Heading>
+                <Text className="self-center" size="xl">{artist}, {year}</Text>
+                <GlueButton>
+                    <ButtonText>Add To Favorites</ButtonText>
+                </GlueButton>
+                <Button title="Go Back" onPress={() => router.back()}/>
+            </Box>
+        </SafeAreaView>
     )
 }
