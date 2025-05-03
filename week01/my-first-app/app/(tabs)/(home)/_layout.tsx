d54fe1af5f7309f67ed8910efd6d02bfa8b168ac
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-03 10:48:07 -0700

    Update header toggleColorMode icon, imports

--- week01/my-first-app/app/(tabs)/(home)/_layout.tsx
+++ week01/my-first-app/app/(tabs)/(home)/_layout.tsx
@@ -1,11 +1,27 @@
-import { Stack } from "expo-router";
+import { ThemeContext } from "@/app/_layout";
+import { Icon, MoonIcon, SunIcon } from "@/components/ui/icon";
+import { Pressable } from "@/components/ui/pressable";
+import { Stack, useRouter } from "expo-router";
+import { useContext } from "react";
 
 export default function HomeLayout() {
+    const router = useRouter();
+    const { colorMode, toggleColorMode } = useContext(ThemeContext);
+
     return (
         <Stack
             screenOptions={{
                 headerShown: true,
-                contentStyle: {backgroundColor: 'transparent'},
+                headerRight: () => (
+                    <Pressable
+                        onPress={toggleColorMode}
+                    >
+                        <Icon
+                            as={colorMode === 'light' ? MoonIcon : SunIcon}
+                            size='xl'
+                        />
+                    </Pressable>
+                )
             }}>
                 <Stack.Screen name="index" options={{ title: "Home"}} />
                 <Stack.Screen name="[title]" options={{ title: "Details"}} />
