c1f5bac7043367327a4b3b1845de211f8876cd06
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-03 18:31:46 -0700

    Create PaintingContextProvider

---
+++ week01/my-first-app/components/ui/painting-contex-provider/index.tsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useState } from "react";
+
+
+export type Painting = {
+    id: string;
+    name: string;
+    artist: string;
+    year: number;
+    wikipediaLink: string;
+    isFavorite?: boolean;
+}
+
+type PaintingContextType = {
+    paintings: Painting[];
+    addPainting: (paintings: Painting) => void;
+    updatePainting: (id: string, updatedPainting: Partial<Painting>) => void;
+    toggleFavorite: (id: string) => void;
+}
+
+const PaintingContext = createContext<PaintingContextType | undefined>(undefined);
+
+export const PaintingProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [paintings, setPaintings] = useState<Painting[]>([]);
+
+    const addPainting = (painting: Painting) => {
+        setPaintings((prev) => [...prev, painting]);
+    };
+
+    const updatePainting = (id: string, updatedPainting: Partial<Painting>) => {
+        setPaintings((prev) =>
+            prev.map((painting) =>
+                painting.id === id ? { ...painting, ...updatedPainting } : painting
+            )
+        );
+    };
+
+    const toggleFavorite = (id: string) => {
+        setPaintings((prev) => 
+            prev.map((painting) =>
+                painting.id === id ? { ...painting, isFavorite: !painting.isFavorite } : painting
+            )
+        );
+    };
+
+    return (
+        <PaintingContext.Provider value={{ paintings, addPainting, updatePainting, toggleFavorite }}>
+            {children}
+        </PaintingContext.Provider>
+    )
+}
+
+export const usePaintingContext = () => {
+    const context = useContext(PaintingContext);
+    if (!context) {
+        throw new Error('usePaintingContext must be used within a PaintingProvider');
+    }
+    return context;
+}
