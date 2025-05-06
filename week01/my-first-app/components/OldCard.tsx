6547100a4c198c40cf661ca694826c3cc411d0a0
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-05 17:35:46 -0700

    Change card to OldCard

--- week01/my-first-app/components/OldCard.tsx
+++ week01/my-first-app/components/OldCard.tsx
@@ -14,7 +14,7 @@ interface CardProps {
     id: string;
 }
 
-const Card: React.FC<CardProps> = ({ name, artist, year, wikipediaLink, id }) => {
+const OldCard: React.FC<CardProps> = ({ name, artist, year, wikipediaLink, id }) => {
       const backgroundColor = useThemeColor({}, 'background');
       const color = useThemeColor({}, 'text');
 
@@ -79,4 +79,4 @@ const styles = StyleSheet.create({
       }
 });
 
-export default Card;
+export default OldCard;
