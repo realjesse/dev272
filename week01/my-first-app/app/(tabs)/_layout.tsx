ded130c21867e710e04fca6f202fe8b489ab1df9
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-05 21:57:40 -0700

    Add FAB

--- week01/my-first-app/app/(tabs)/_layout.tsx
+++ week01/my-first-app/app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from 'expo-router';
+import { router, Tabs, useRouter } from 'expo-router';
 import React, { useContext } from 'react';
 import { Platform } from 'react-native';
 
@@ -9,50 +9,63 @@ import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Button, ButtonText } from '@/components/ui/button';
 import { ThemeContext } from '../_layout';
+import { Box } from '@/components/ui/box';
+import { Fab, FabIcon } from '@/components/ui/fab';
+import { EditIcon } from '@/components/ui/icon';
 
 export default function TabLayout() {
+  const router = useRouter();
   const colorScheme = useColorScheme();
   const { toggleColorMode } = useContext(ThemeContext);
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-              // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="(home)"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-          headerRight: () => (
-            <Button onPress={toggleColorMode}>
-              <ButtonText>Toggle Theme</ButtonText>
-            </Button>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="favorites"
-        options={{
-          title: 'Favorites',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-          headerRight: () => (
-            <Button onPress={toggleColorMode}>
-              <ButtonText>Toggle Theme</ButtonText>
-            </Button>
-          ),
-        }}
-      />
-    </Tabs>
+    <Box className='flex-1'>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+          headerShown: false,
+          tabBarButton: HapticTab,
+          tabBarBackground: TabBarBackground,
+          tabBarStyle: Platform.select({
+            ios: {
+                // Use a transparent background on iOS to show the blur effect
+              position: 'absolute',
+            },
+            default: {},
+          }),
+        }}>
+        <Tabs.Screen
+          name="(home)"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+            headerRight: () => (
+              <Button onPress={toggleColorMode}>
+                <ButtonText>Toggle Theme</ButtonText>
+              </Button>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="favorites"
+          options={{
+            title: 'Favorites',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+            headerRight: () => (
+              <Button onPress={toggleColorMode}>
+                <ButtonText>Toggle Theme</ButtonText>
+              </Button>
+            ),
+          }}
+        />
+      </Tabs>
+      <Fab
+        size='lg'
+        className='bottom-32 dark:bg-zinc-700'
+        onPress={() => router.navigate('/add-painting')}
+      >
+        <FabIcon as={EditIcon} color='white'/>
+      </Fab>
+    </Box>
   );
 }
