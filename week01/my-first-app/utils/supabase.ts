95418a626427c4f2639f4d6bb9a1d06f5f930bad
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-12 19:31:58 -0700

    Link supabase

--- week01/my-first-app/utils/supabase.ts
+++ week01/my-first-app/utils/supabase.ts
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL || "",
-  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
+  "https://akzmutrbyxuvyrzsguay.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrem11dHJieXh1dnlyenNndWF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwOTMzMTMsImV4cCI6MjA2MjY2OTMxM30.X1kIAH_1hgfswIz2reTg9gFkKBYZYYSJaquv6h2iTKc",
   {
     auth: {
       storage: AsyncStorage,
