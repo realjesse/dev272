b6a815017fc2e238114a45d4490654d97309ace3
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-13 12:58:25 -0700

    Create getPaitnings hook

---
+++ week01/my-first-app/hooks/useGetPaintings.ts
@@ -0,0 +1,20 @@
+import { supabase } from "@/utils/supabase"
+import { useQuery } from "@tanstack/react-query"
+
+
+
+export const useGetPaintings = () => {
+    return useQuery({
+        queryKey: ['paintings'],
+        queryFn: async () => {
+            const { data, error } = await supabase
+                .from('paintings')
+                .select('*')
+            if (error) {
+                throw new Error(error.message)
+            }
+            return data
+        },
+        staleTime: 1000 * 60 * 5,
+    })
+}
