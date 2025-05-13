a0ea9c15b65154fec9fcf4c56c80f0a70d18e576
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-13 12:35:46 -0700

    Wrap app in query provider

--- week01/my-first-app/app/_layout.tsx
+++ week01/my-first-app/app/_layout.tsx
@@ -9,6 +9,9 @@ import { createContext, useEffect, useState } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { PaintingProvider } from '@/components/ui/painting-contex-provider';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppState } from 'react-native';
+import { supabase } from '@/utils/supabase';
 
 type ThemeContextType = {
   colorMode: ModeType;
@@ -20,11 +23,22 @@ export const ThemeContext = createContext<ThemeContextType>({
   toggleColorMode: () => {},
 });
 
+const queryClient = new QueryClient();
+
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
+})
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [colorMode, setColorMode] = useState<ModeType>("light");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -44,17 +58,41 @@ export default function RootLayout() {
     setColorMode((prev) => (prev === "light" ? "dark" : "light"));
   }
 
+  // handle initial superbase auth
+  useEffect(() => {
+    if (isAuthenticated) {
+      return;
+    }
+
+    const autoSignin = async () => {
+      const {data, error} = await supabase.auth.signInWithPassword({
+        email: "test@dev.com",
+        password: "testtest",
+      });
+      if (error) {
+        console.error("Error signing in:", error);
+      } else {
+        setIsAuthenticated(true);
+        console.log("Signed in user:", data);
+      }
+    }
+
+    autoSignin();
+  }, [isAuthenticated])
+
   return (
-    <GluestackUIProvider mode={colorMode}>
-      <ThemeContext.Provider value={{colorMode, toggleColorMode}}>
-        <PaintingProvider>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-        </PaintingProvider>
-        <StatusBar style="auto" />
-      </ThemeContext.Provider>
-    </GluestackUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <GluestackUIProvider mode={colorMode}>
+        <ThemeContext.Provider value={{colorMode, toggleColorMode}}>
+          <PaintingProvider>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="+not-found" />
+            </Stack>
+          </PaintingProvider>
+          <StatusBar style="auto" />
+        </ThemeContext.Provider>
+      </GluestackUIProvider>
+    </QueryClientProvider>
   );
 }
